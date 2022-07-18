aea705f71f79897a4e3746f73847c2d0a670fba8
sujoy <sujoy.banik@test.com>
2022-07-18 15:54:36 +0530

    first commit

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Greeter {
+  public String sayHello() {
+    return "Hello world- This is Pavan Wankhade your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
+  }
+}
