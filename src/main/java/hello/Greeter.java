ae776321ad42e628b1e28a3a4f76ca47f33f736b
sujoyb01 <100847590+sujoyb01@users.noreply.github.com>
2022-07-18 18:28:32 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is Sujoy your Trainer !! Hi or  is the string needed here.Testing Pipeline";
+    return "Hello world- This is Sujoy your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
   }
 }
